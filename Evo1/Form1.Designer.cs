3e68f97a13f6acf26e35c6e475b9c747b5e107a9
Bogdan <boggzon@gmail.com>
2024-04-18 15:59:19 +0300

    first commit

---
+++ Evo1/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace Evo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            textBox2 = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            textBox3 = new System.Windows.Forms.TextBox();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            textBox4 = new System.Windows.Forms.TextBox();
+            label4 = new System.Windows.Forms.Label();
+            button4 = new System.Windows.Forms.Button();
+            button5 = new System.Windows.Forms.Button();
+            button6 = new System.Windows.Forms.Button();
+            textBox5 = new System.Windows.Forms.TextBox();
+            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            label7 = new System.Windows.Forms.Label();
+            label8 = new System.Windows.Forms.Label();
+            label9 = new System.Windows.Forms.Label();
+            button7 = new System.Windows.Forms.Button();
+            label10 = new System.Windows.Forms.Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(104, 313);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(206, 32);
+            button1.TabIndex = 0;
+            button1.Text = "Конвертировать в двоичное";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new System.Drawing.Point(104, 272);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(231, 254);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(62, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Двоичное";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new System.Drawing.Point(104, 228);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(100, 23);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "10";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(111, 254);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(72, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Десятичное";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(120, 210);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(78, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Размерность";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new System.Drawing.Point(210, 272);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(100, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(104, 351);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(206, 32);
+            button2.TabIndex = 7;
+            button2.Text = "Конвертировать в десятичное";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(353, 73);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(326, 112);
+            button3.TabIndex = 8;
+            button3.Text = "Генерация";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new System.Drawing.Point(364, 44);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(100, 23);
+            textBox4.TabIndex = 9;
+            textBox4.Text = "10";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new System.Drawing.Point(353, 26);
+            label4.Name = "label4";
+            label4.Size = new System.Drawing.Size(111, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Размер популяции";
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(353, 205);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(326, 112);
+            button4.TabIndex = 11;
+            button4.Text = "Селекция";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new System.Drawing.Point(353, 334);
+            button5.Name = "button5";
+            button5.Size = new System.Drawing.Size(326, 112);
+            button5.TabIndex = 12;
+            button5.Text = "Скрещивание";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new System.Drawing.Point(353, 461);
+            button6.Name = "button6";
+            button6.Size = new System.Drawing.Size(326, 112);
+            button6.TabIndex = 13;
+            button6.Text = "Мутация";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new System.Drawing.Point(688, 380);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(100, 23);
+            textBox5.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new System.Drawing.Point(685, 360);
+            label5.Name = "label5";
+            label5.Size = new System.Drawing.Size(117, 15);
+            label5.TabIndex = 15;
+            label5.Text = "Точка скрещивания";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new System.Drawing.Point(826, 360);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(117, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Точка скрещивания";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new System.Drawing.Point(826, 388);
+            label7.Name = "label7";
+            label7.Size = new System.Drawing.Size(117, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Точка скрещивания";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new System.Drawing.Point(958, 360);
+            label8.Name = "label8";
+            label8.Size = new System.Drawing.Size(117, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Точка скрещивания";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new System.Drawing.Point(958, 388);
+            label9.Name = "label9";
+            label9.Size = new System.Drawing.Size(117, 15);
+            label9.TabIndex = 19;
+            label9.Text = "Точка скрещивания";
+            // 
+            // button7
+            // 
+            button7.Location = new System.Drawing.Point(742, 488);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(158, 58);
+            button7.TabIndex = 20;
+            button7.Text = "Ответ";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new System.Drawing.Point(919, 510);
+            label10.Name = "label10";
+            label10.Size = new System.Drawing.Size(117, 15);
+            label10.TabIndex = 21;
+            label10.Text = "Точка скрещивания";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1103, 594);
+            Controls.Add(label10);
+            Controls.Add(button7);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox5);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Evo1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evo1
+{
+    public partial class Form1 : Form
+    {
+        public string[] population;
+
+        public double[] sectors;
+
+        public string ConverterToBinary(int number, int length)
+        {
+            string digit = Convert.ToString(number, 2);
+            if (digit.Length < length)
+            {
+                digit = digit.PadLeft(length, '0');
+            }
+            if (digit.Length > length)
+            {
+                digit = digit.Substring(digit.Length - length);
+            }
+            return digit;
+        }
+
+        public void Generation(int num)
+        {
+            population = new string[num];
+            for (int i = 0; i < num; i++)
+            {
+                Random number = new Random();
+                string osob = ConverterToBinary(number.Next(), Convert.ToInt32(textBox2.Text));
+                population[i] = osob;
+            }
+        }
+
+        public double func(int x)
+        {
+            return (double)Math.Sin(x / 4) + 1.5;
+        }
+
+        public void Selection()
+        {
+            Random rand = new Random();
+            sectors = new double[Convert.ToInt32(textBox2.Text)];
+            sectors[0] = func(Convert.ToInt32(population[0]));
+            for (int i = 1; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                sectors[i] = sectors[i - 1] + func(ConverterToDecimal(population[i]));
+            }
+
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                sectors[i] /= sectors[Convert.ToInt32(textBox2.Text) - 1];
+            }
+
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                double randomValue = rand.NextDouble();
+                for (int j = 0; j < Convert.ToInt32(textBox2.Text); j++)
+                {
+                    if (randomValue < sectors[j])
+                    {
+                        population[i] = population[j];
+                        j = Convert.ToInt32(textBox2.Text);
+                    }
+                }
+            }
+            int stop = 0;
+        }
+
+        public void Crossing(int point)
+        {
+            for (int i = 1; i < Convert.ToInt32(textBox2.Text); i += 2)
+            {
+                StringBuilder part1 = new StringBuilder(population[i - 1]);
+                StringBuilder part2 = new StringBuilder(population[i]);
+                for (int j = point - 1; j < population[i].Length; j++)
+                {
+                    part1[j] = population[i][j];
+                    part2[j] = population[i - 1][j];
+                }
+                population[i - 1] = part1.ToString();
+                population[i] = part2.ToString();
+            }
+        }
+
+        public void Mutation()
+        {
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                StringBuilder sb = new StringBuilder(population[i]);
+                for (int j = 0; j < sb.Length; j++)
+                {
+                    if (sb[j] == '0')
+                        sb[j] = '1';
+                    else
+                        sb[j] = '0';
+                }
+                population[i] = sb.ToString();
+            }
+        }
+
+        public int Result()
+        {
+            int result = 0;
+            double max = 0;
+            for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                if (max <= func(ConverterToDecimal(population[i])))
+                {
+                    max = func(ConverterToDecimal(population[i]));
+                    result = ConverterToDecimal(population[i]);
+                }
+            }
+            return result;
+        }
+
+        public int ConverterToDecimal(string digit)
+        {
+            return Convert.ToInt32(digit, 2);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(textBox1.Text);
+            int length = Convert.ToInt32(textBox2.Text);
+            string digit = ConverterToBinary(number, length);
+            textBox3.Text = digit;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ConverterToDecimal(textBox3.Text).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Generation(Convert.ToInt32(textBox4.Text));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Selection();
+            label6.Text = population[0];
+            label7.Text = population[1];
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Crossing(Convert.ToInt32(textBox5.Text));
+            label8.Text = population[0];
+            label9.Text = population[1];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Mutation();
+            label6.Text = population[0];
+            label7.Text = population[1];
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            label10.Text = Result().ToString();
+        }
+    }
+}
